e1d744bf533ab3d67e8f4eefe80e5bd3e4f1b5e7
Paul Sonnentag <paul.sonnentag@googlemail.com>
2026-04-19 13:18:41 +0100

    remove sanity demo and design sketch

--- packages/demo/sanity-tools/comp-a/comp-a.js
+++
@@ -1,8 +0,0 @@
-export default function (root) {
-  root.style.padding = "16px";
-  root.style.background = "white";
-  const heading = root.ownerDocument.createElement("h1");
-  heading.appendChild(root.ownerDocument.createTextNode("Hello"));
-  root.appendChild(heading);
-  return () => {};
-}
